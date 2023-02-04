ae99658a650cff4e91cdbde3e613aa518ca55d82
mwisnunugraha <mwisnunugraha1@gmail.com>
2023-02-04 22:06:16 +0800

    update .gitignore and requirements update

--- create_file.py
+++ create_file.py
@@ -8,23 +8,21 @@ app = Flask(__name__)
 def welcome():
     return {"Status": True, "Nama": "belajar_python"}
 
-@app.route('/create_file', methods=['POST'])
+@app.route('/create_file', methods=['POST','GET'])
+
 def create_file():
     file_content = request.form.get('file_content')
     file_name = request.form.get('file_name')
     
-    with open(file_name, "w") as file:
-        file.write(file_content)
+    if methods == 'GET' :
+        return {'status': False}
+    if methods == 'POST' :
+        
+        with open(file_name, "w") as file:
+            file.write(file_content)
     
-    return f"File '{file_name}' created successfully!"
+        return f"File '{file_name}' created successfully!"
 
 if __name__ == '__main__':
     app.run(debug=True)
     serve(app, host='0.0.0.0', port=8080, threads=1)
-
-methodes =['POST']
-
-if methods  POST:
-    print('created succesfully')
-else:
-    print('false')
