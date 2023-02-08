30e3b69685f5cb97a3658825f2cf48a9c0e4e17d
rafli024 <muhammadrafli2460@gmail.com>
2023-02-08 09:57:33 +0700

    optimize code and add comment

--- create_file.py
+++ create_file.py
@@ -1,50 +1,51 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from waitress import serve
-from datetime import datetime
 import datetime
-<<<<<<< HEAD
-import pandas as pd
-import pathlib
-=======
->>>>>>> 4de62534c0cbbfd7dc25db5dde9ccb03754c97ca
+import os
 
+# global variable used by all function that call it
 app = Flask(__name__)
 current_time = datetime.datetime.now()
 time_str = current_time.strftime("%Y-%m-%d %H:%M:%S")
 
-<<<<<<< HEAD
-current_time = datetime.datetime.now()
-time_str = current_time.strftime("%Y-%m-%d %H:%M:%S")
-df = pd.DataFrame({'date':['07/02/2023']})
-file = pathlib.Path('file_name')
-file.unlink
-
-=======
->>>>>>> 4de62534c0cbbfd7dc25db5dde9ccb03754c97ca
+# Get status if this function and route been called
 @app.route('/', methods=['GET'])
 def welcome():
     return {"Status": True, "Nama": "belajar_python", "Time": time_str}
 
-
+# create file name with content file write after post it
+# and adding error if something doesn't right
 @app.route('/create_file', methods=['POST','GET'])
 def create_file():
     file_content = request.form.get('file_content')
     file_name = request.form.get('file_name')
+    message = "File {} has been created".format(file_name)
     
     if request.method == 'GET' :
         return {'status': False}
-        
-    if request.method == 'POST' :
-        with open(file_name, "w") as file:
-            file.write(file_content)
     
-<<<<<<< HEAD
-        return f"File '{file_name}' created successfully!\n Time completion {time_str}"
-=======
-        return f"File '{file_name}' created successfully!\n Time completion: {time_str}"
->>>>>>> 4de62534c0cbbfd7dc25db5dde9ccb03754c97ca
+    try:
+        if request.method == 'POST' :
+            with open(file_name, "w") as file:
+                file.write(file_content)
+            return jsonify({"message": message, "Timestamp": time_str}), 200
+    except Exception as e:
+        return jsonify({"Error": str(e)}), 500
 
 
+# delete file name with extension, return with message if success
+# and return false with error if file doesn't exist
+@app.route('/delete_file', methods=['POST'])
+def delete_file():
+    file_name = request.form.get('file_name')
+    message = "File {} has been deleted".format(file_name)
+    try:
+        os.remove(file_name)
+        return jsonify({"message": message, "Timestamp": time_str}), 200
+    except Exception as e:
+        return jsonify({"Error": str(e)}), 500
+
+# main function
+# it means that code enclosed  in if __name__ == "__main__": block will only be executed if the file is run as the main program
 if __name__ == '__main__':
-    app.run(debug=True)
-    serve(app, host='0.0.0.0', threads=1)
+    serve(app, host='0.0.0.0', port=8080, threads=1)
