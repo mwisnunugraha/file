3ab5f26067ac604db60812744473660bab23eacb
mwisnunugraha <mwisnunugraha1@gmail.com>
2023-02-16 23:04:19 +0800

    belajar json

--- latihan_json/data_sekolah.py
+++ latihan_json/data_sekolah.py
@@ -1,5 +1,11 @@
-from flask import Flask 
+from flask import Flask, request, json
 import json
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def welcome():
+    return {"Status": True, "Nama": "belajar_json"}
 
 #buat buka file json
 file_json = open("data_sekolah.json")
@@ -7,5 +13,11 @@ file_json = open("data_sekolah.json")
 # prsing data json
 data =  json.loads(file_json.read())
 
-# menampilkan isi data json
-print(data)
+
+@app.route('/data_sekolah.json')
+def tampilkan_json():
+    return 'data_sekolah.json'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    serve(app, host='0.0.0.0', port=8080, threads=1)
