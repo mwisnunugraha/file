c0cc74b05057e87c75e6573eb6806324246e72d8
rafli024 <muhammadrafli2460@gmail.com>
2023-03-10 11:13:24 +0700

    re-write code to create more useful calculator

--- kalkulator/kalkulator.py
+++ kalkulator/kalkulator.py
@@ -1,14 +1,42 @@
-print(
-"""   
-PENJUMLAHAN = +
-PENGURANGAN = -
-KELUAR PROGRAM = exit
-"""
-)
+# print(
+# """   
+# PENJUMLAHAN = +
+# PENGURANGAN = -
+# KELUAR PROGRAM = exit
+# """
+# )
 
-while True:
-    try:
-        masukanPerintah = eval(input("Masukan perintah"))
-        print(f"Hasil : {masukanPerintah}")
-    except Exception as e:
-            print("Input yang benar")
+
+def pertambahan(angka1, angka2):
+    return angka1 + angka2
+
+if __name__ == "__main__":
+    fungsi_kalkulator = {
+        1: pertambahan
+    }
+
+    print("""
+    Selamat datang di kalkulator sederhana.
+    masukkan operasi yang diinginkan:
+    1. tambah
+    """)
+
+    user_input = int(input("Masukkan operasi yang anda inginkan: "))
+    angka1 = int(input("masukkan angka 1: "))
+    angka2 = int(input("masukkan angka 2: "))
+
+    if user_input not in fungsi_kalkulator:
+        print("invalid input")
+        exit()
+
+    hasil = fungsi_kalkulator[user_input](angka1, angka2)
+    print ("Hasil : ",hasil)
+
+
+
+# while True:
+#     try:
+#         masukanPerintah = eval(input("Masukan perintah"))
+#         print(f"Hasil : {masukanPerintah}")
+#     except Exception as e:
+#             print("Input yang benar")
